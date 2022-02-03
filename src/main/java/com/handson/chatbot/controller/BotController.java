4544b5eb72a423464f0198858f93f29ebaad0169
Niv Itzhaky <niv.itzhaky@gmail.com>
2022-02-03 23:29:06 +0200

    with weather

--- src/main/java/com/handson/chatbot/controller/BotController.java
+++ src/main/java/com/handson/chatbot/controller/BotController.java
@@ -1,10 +1,10 @@
 package com.handson.chatbot.controller;
 
 import com.handson.chatbot.service.AmazonService;
+import com.handson.chatbot.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -16,8 +16,17 @@ public class BotController {
     @Autowired
     AmazonService amazonService;
 
+    @Autowired
+    WeatherService weatherService;
+
     @RequestMapping(value = "/amazon", method = RequestMethod.GET)
-    public ResponseEntity<?> getOneStudent(@RequestParam String keyword) throws IOException {
+    public ResponseEntity<?> getProduct(@RequestParam String keyword) throws IOException {
         return new ResponseEntity<>(amazonService.searchProducts(keyword), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/weather", method = RequestMethod.GET)
+    public ResponseEntity<?> getWeather(@RequestParam String location) throws IOException {
+        return new ResponseEntity<>(weatherService.searchWeather(location), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/handson/chatbot/service/WeatherService.java
+++ src/main/java/com/handson/chatbot/service/WeatherService.java
@@ -1,145 +1,32 @@
-# chatbot
+package com.handson.chatbot.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-### swagger
-pom.xml
-<br>
-<version>2.6.2</version> -> <version>2.5.2</version>
-```
-		<dependency>
-			<groupId>io.springfox</groupId>
-			<artifactId>springfox-swagger-ui</artifactId>
-			<version>2.6.1</version>
-		</dependency><!-- https://mvnrepository.com/artifact/io.springfox/springfox-swagger2 -->
-		<dependency>
-			<groupId>io.springfox</groupId>
-			<artifactId>springfox-swagger2</artifactId>
-			<version>2.6.1</version>
-		</dependency>
-```
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-config/SwaggerConfig.java
-```java
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-}
-```
-controller/BotController.java
-```java
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-}
-```
-service/AmazonService.java
-```java
 @Service
-public class AmazonService {
+public class WeatherService {
 
-    public String searchProducts(String keyword) {
-        return "Searched for:" + keyword;
-    }
-}
-```
-controller/BotController.java
-```java
-@Service
-@RestController
-@RequestMapping("/bot")
-public class BotController {
+    public static final OkHttpClient client = new OkHttpClient();
 
     @Autowired
-    AmazonService amazonService;
+    ObjectMapper om;
 
-    @RequestMapping(value = "/amazon", method = RequestMethod.GET)
-    public ResponseEntity<?> getOneStudent(@RequestParam String keyword)
-    {
-        return new ResponseEntity<>(amazonService.searchProducts(keyword), HttpStatus.OK);
-    }
-}
-```
-http://localhost:8080/swagger-ui.html#
-<br>
-commit - with swagger
-###Find the search api
-https://www.amazon.com/s?k=ipod
-<br>
-service/AmazonService.java
-```java
-    private String getProductHtml(String keyword) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://www.amazon.com/s?k=" + keyword +"&crid=1FVOAPA9AELRZ&sprefix=ipod%2Caps%2C181&ref=nb_sb_noss_1")
-                .method("GET", null)
-                .addHeader("authority", "www.amazon.com")
-                .addHeader("cache-control", "max-age=0")
-                .addHeader("rtt", "100")
-                .addHeader("downlink", "10")
-                .addHeader("ect", "4g")
-                .addHeader("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"98\", \"Google Chrome\";v=\"98\"")
-                .addHeader("sec-ch-ua-mobile", "?0")
-                .addHeader("sec-ch-ua-platform", "\"macOS\"")
-                .addHeader("upgrade-insecure-requests", "1")
-                .addHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36")
-                .addHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-                .addHeader("sec-fetch-site", "same-origin")
-                .addHeader("sec-fetch-mode", "navigate")
-                .addHeader("sec-fetch-user", "?1")
-                .addHeader("sec-fetch-dest", "document")
-                .addHeader("referer", "https://www.amazon.com/")
-                .addHeader("accept-language", "en-US,en;q=0.9,he;q=0.8")
-                .addHeader("cookie", "session-id=131-3987483-5342545; session-id-time=2082787201l; i18n-prefs=USD; sp-cdn=\"L5Z9:IL\"; skin=noskin; ubid-main=133-9581942-3780819; session-token=9wuRyGDfGYRNIQddpLSfu/7ZXPkW7pQoYBvMwmGsIhnfY51Iqj+MGS377K23lkwtdjhFx7pU6lcOwFpb0U6WneR7bB7A16Fx7VbQbOEVXnmyIo1i6GXJCcO4dDDR8dN3t5hl/PRi/+vWZPCxL7pYX7A0IOLl74C6MykvrYSxmA29bWSf8tfTif60UEjxD0kk; csm-hit=tb:TTHKG9BMH5E1XG8YFBGP+s-PMZSXATEMDSYE2A3YAS8|1643843190534&t:1643843190534&adb:adblk_yes; session-token=\"2xJZqprgyjzxD7aWv5lvF6L2Jj6wbj75+0bl0405+iQ/teLGzDnuRJLFAIT+TZQlkgfSAzqMtgIwEeHBFVD0aip6GkUe+j61VFbW6p2RqtI4ZpKzPhLXUan7/XN3IT54f1XxTmwQcSjyB2GQ/9kA6/4K7i8kFxf9gYWBIaSqklC4vZX3A8WEtWGUcQxKUx1PP7imQpBv5nT5pijsa/fk2A==\"")
-                .build();
-        Response response = client.newCall(request).execute();
-        return response.body().string();
-    }
-```
-commit - with search product call
-
-### find the regex:
-https://regex101.com/
-<br>
-service/AmazonService.java
-```java
-    public static final Pattern PRODUCT_PATTERN = Pattern.compile("<span class=\\\"a-size-medium a-color-base a-text-normal\\\">([^<]+)</span> </a> </h2></div><div class=\\\"a-section a-spacing-none a-spacing-top-micro\\\"><div class=\\\"a-row a-size-small\\\"><span aria-label=\\\"([^\\\"]+)\\\"><span.*<span class=\\\"a-offscreen\\\">([^<]+)</span>");
-
-    public String searchProducts(String keyword) throws IOException {
-        return parseProductHtml(getProductHtml(keyword));
-    }
-
-    private String parseProductHtml(String html) {
-        String res = "";
-        Matcher matcher = PRODUCT_PATTERN.matcher(html);
-        while (matcher.find()) {
-            res += matcher.group(1) + " - " + matcher.group(2) + ", price:" + matcher.group(3) + "<br>\n";
-        }
+    public String searchWeather(String keyword) throws IOException {
+        Integer locationId = getWeatherLocationId(keyword);
+        String res = getWeatherForLocationId(locationId);
         return res;
     }
-```
-commit - with product regex
-### weather search
-https://www.yahoo.com/news/weather/
-<br>
 
-service/WeatherService.java
-```java
+
     private Integer getWeatherLocationId(String keyword) throws IOException {
         Request request = new Request.Builder()
                 .url("https://www.yahoo.com/news/_tdnews/api/resource/WeatherSearch;text=" + keyword +"?bkt=%5B%22a2-plutus-rr-backbucket%22%2C%22xray-us-nel-about-03%22%2C%22xray-exp-api-ctrl%22%2C%22JARVISUSNEWSDESK-INSESSION-00%22%5D&device=desktop&ecma=modern&feature=cacheContentCanvas%2CdelayCacheHeaders%2CdisableCommentsMessage%2CenableCCPAFooter%2CenableCMP%2CenableConsentData%2CenableGDPRFooter%2CenableGuceJs%2CenableGuceJsOverlay%2Clivecoverage%2CnewContentAttribution%2CnewLogo%2CrivendellMigration%2Cuserintent%2CvideoDocking%2CoathPlayer%2CenableAdlite&intl=us&lang=en-US&partner=none&prid=fpmog1tgvn0u5&region=US&site=fp&tz=Asia%2FJerusalem&ver=0.0.12106063&returnMeta=true")
@@ -198,8 +85,6 @@ service/WeatherService.java
             return "Not Found";
         }
     }
-```
-```java
 
     static class WeatherLocationData {
         Weathers data;
@@ -240,5 +125,22 @@ service/WeatherService.java
         }
     }
 
-```
-commit - with weather
+    static class WeatherLocations {
+        List<WeatherLocation> data;
+
+        public List<WeatherLocation> getData() {
+            return data;
+        }
+    }
+
+    static class WeatherLocation {
+        Integer woeid;
+
+        public Integer getWoeid() {
+            return woeid;
+        }
+    }
+
+
+
+}
